68f5aac9666e57c20732e6e8d6e44d4d11795658
salmamyoussef <salma2018170181@cis.asu.edu.eg>
2022-02-14 16:03:12 +0200

    TTTTTTT

--- src/program/Main.java
+++ src/program/Main.java
@@ -6,14 +6,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		/*
+		
 		try {
 			Test.test();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		*/
+		
 		
 		
 	}
