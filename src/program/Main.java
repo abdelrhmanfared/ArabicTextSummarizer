cf4b07c3aade5241369f4e9644f9cd1298d04772
abdullahalaa12 <abdullah.alaa12@gmail.com>
2022-02-14 12:37:38 +0200

    Adding basic classes

---
+++ src/program/Main.java
@@ -0,0 +1,21 @@
+package program;
+
+import test.Test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		try {
+			Test.test();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		*/
+		
+		
+	}
+
+}
