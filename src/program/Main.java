aef50ce08b70d1a32876de1ad3b63fd8a7ee5b9f
abdullahalaa12 <abdullah.alaa12@gmail.com>
2022-02-14 16:09:08 +0200

    Preprocessing

--- src/program/Main.java
+++ src/program/Main.java
@@ -1,5 +1,11 @@
 package program;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+import preprocessing.Preprocessing;
 import test.Test;
 
 public class Main {
@@ -15,7 +21,31 @@ public class Main {
 		}
 		*/
 		
-		
+		String ArabicText = readfile("ArabicText.txt");
+		try {
+			Preprocessing pre = new Preprocessing(ArabicText);
+			System.out.print(pre.getRootText());
+		} catch (ClassNotFoundException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+	
+	public static String readfile(String filename) {
+	    try {
+	      File myObj = new File(filename);
+	      Scanner myReader = new Scanner(myObj);
+	      myReader.useDelimiter("\\Z");
+	      String data = myReader.next();
+	      myReader.close();
+	      return data;
+	    		  
+	    } catch (FileNotFoundException e) {
+	      System.out.println("An error occurred.");
+	      e.printStackTrace();
+	      return null;
+	    }
+	    
+	  }
 
 }
