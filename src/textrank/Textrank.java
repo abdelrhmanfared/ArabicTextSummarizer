84b266035b590c6d7c8ba76f3a0f606271396696
abdelrhmanfared <69286853+abdelrhmanfared@users.noreply.github.com>
2022-02-16 23:10:56 +0200

    upd

--- src/textrank/Textrank.java
+++ src/textrank/Textrank.java
@@ -1,12 +1,16 @@
 package textrank;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import preprocessing.Preprocessing;
+import utilities.AraNormalizer;
 
 public class Textrank {
 	private Preprocessing pre;
 	private String summarizedText;
+	AraNormalizer arn = new AraNormalizer();
 	
 	public String getSummarizedText()
 	{
@@ -52,25 +56,51 @@ public class Textrank {
 		return 0;
 	}
 	
-	//Words in the sentence such as “therefore, finally and thus” can be a good indicators of significant content
+	//Words in the sentence such as â€œtherefore, finally and thusâ€� can be a good indicators of significant content
 	private int cueWords()
 	{
 		return 0;
 	}
 	
-	//Words that are used to emphasize or focus on special idea such as ”have outstanding, and support for”
-	private int positiveKeyWords()
-	{
-		return 0;
-	}
+	//Words that are used to emphasize or focus on special idea such as â€�have outstanding, and support forâ€�
+	private double [] positiveKeyWords(String [] sentance)
+	{		
+		String [] Postive_Words = {
+				"ايد","تأييد ","اقر","اقرار","اثبت","اثبات","المثبت من","تحديد","تأكيد","دليل","بينة ","ايجاب","أدلة"
+				,"المؤكد من","بالتأكيد","أكد","وثق","توكيد","برهان","شهادة","جزم","تقرير ","تحقيق" 
+				,"تقرير", "جزم", "شهادة", "برهان", "توكيد", "من المصدق", "تصديق",		
+				"صدق", "دلل", "حدد", "ح قق", "برهن", "شهد", "ذو معنى", "كل المعنى", "داللي"
+		};
+		for (String str : Postive_Words) {
+			str = arn.normalize(str);
+		}
+		
+		int Total = 0 , freq[] = new int [sentance.length];
+		for (int i=0;i<sentance.length;i++) {
+			for (int j=0;j<Postive_Words.length;j++) {
+				Pattern P1 =  Pattern.compile(".*\\b"+Postive_Words[j]+"\\b.*");
+				Matcher M = P1.matcher(sentance[i]);
+				while(M.find()) {	
+					Total++;
+					freq[i]++;
+				}
+			}
+		}	
+			double Sentance_Score [] = new double[sentance.length];
+			if (Total == 0)return Sentance_Score;
+			for (int i=0;i<sentance.length;i++) {
+				Sentance_Score[i] = ((double)(freq[i])/Total);
+			}
+			return Sentance_Score;
+		}
 	
 	//Existence of numerical data in the sentence.
 	private int sentenceInclusionOfNumericalData()
 	{
-		return 0;
+		
 	}
 	
-	//Words that serve as an explanation words such as “for example”
+	//Words that serve as an explanation words such as â€œfor exampleâ€�
 	private int occurrenceOfNonEssentialInformation()
 	{
 		return 0;
