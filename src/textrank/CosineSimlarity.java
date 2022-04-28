3f4cbf2ccd239db42cbba6c244ff32444ae23fa4
abdullahalaa12 <abdullah.alaa12@gmail.com>
2022-04-28 11:50:25 +0200

    adding tareq's code

---
+++ src/textrank/CosineSimlarity.java
@@ -0,0 +1,69 @@
+package textrank;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import utilities.TrainedTokenizer;
+
+class CosineSimlarity {
+	public double[][] BOW(String[] sentences, String[] tokens) throws FileNotFoundException, IOException {
+		HashSet<String> BOW = new HashSet<String>();
+		for (String token : tokens)
+			BOW.add(token);
+		Map<String, ArrayList<Pair<String, Integer>>> BowMatrix = new HashMap<String, ArrayList<Pair<String, Integer>>>();
+		int termCount = 0;
+		for (String BOWord : BOW) {
+			for (int i = 0; i < sentences.length; i++) {
+				termCount = termCount(sentences[i], BOWord);
+				if (!BowMatrix.containsKey(sentences[i])) {
+					ArrayList<Pair<String, Integer>> list = new ArrayList<Pair<String, Integer>>();
+					list.add(new Pair<String, Integer>(BOWord, termCount));
+					BowMatrix.put(sentences[i], list);
+				} else
+					BowMatrix.get(sentences[i]).add(new Pair<String, Integer>(BOWord, termCount));
+			}
+		}
+
+		double[][] cosineBOW = new double[sentences.length][sentences.length];
+		for (int i = 0; i < sentences.length; i++) {
+			for (int j = 0; j < sentences.length; j++) {
+				if (i == j)
+					cosineBOW[i][j] = 1.0;
+				else if (cosineBOW[j][i] != 0)
+					cosineBOW[i][j] = cosineBOW[j][i];
+				else {
+					double top = 0;
+					double bottom = 0;
+					double bottom1 = 0;
+					double bottom2 = 0;
+
+					for (int k = 0; k < BowMatrix.get(sentences[i]).size(); k++) {
+						top += (BowMatrix.get(sentences[i]).get(k).getValue()
+								* BowMatrix.get(sentences[j]).get(k).getValue());
+						bottom1 += Math.pow(BowMatrix.get(sentences[i]).get(k).getValue(), 2.0);
+						bottom2 += Math.pow(BowMatrix.get(sentences[j]).get(k).getValue(), 2.0);
+					}
+					bottom = Math.sqrt(bottom1) * Math.sqrt(bottom2);
+					cosineBOW[i][j] = top / bottom;
+				}
+			}
+		}
+		return cosineBOW;
+	}
+
+	public int termCount(String sentence, String term) throws FileNotFoundException, IOException {
+		TrainedTokenizer tokenizer = new TrainedTokenizer();
+		String[] tokens = tokenizer.tokenize(sentence);
+		int counter = 0;
+
+		for (String token : tokens)
+			if (term.equals(token.toString()))
+				counter++;
+
+		return counter;
+	}
+}
--- src/textrank/Textrank.java
+++ src/textrank/Textrank.java
@@ -1,12 +1,15 @@
 package textrank;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 
 import preprocessing.Preprocessing1;
 import utilities.StanfordPOSTagger;
@@ -347,89 +350,39 @@ public class Textrank {
 
 	// The similarity or the overlapping between a sentence and other sentences in
 	// the document
-	private double[] sentenceCentrality(String[] sentences, String[] tokens, String[][] senTokens) {
-		Set<String> words = new HashSet<String>();
-		for (String str : tokens) {
-			words.add(str);
+private double[] sentenceCentrality(String[] sentences, String[] tokens, String[][] senTokens) throws FileNotFoundException, IOException {
+		
+		double Threshold = 0.2;
+		double [][] CosineSimMatrix;
+		double []CentralityMatrix = new double[sentences.length];
+		CosineSimlarity cosSim = new CosineSimlarity();
+		CosineSimMatrix = cosSim.BOW(sentences, tokens);
+		for(int i=0;i<CosineSimMatrix.length;i++)
+		{
+			for(int j=0;j<CosineSimMatrix[i].length;j++)
+				System.out.print(CosineSimMatrix[i][j]+" ");
+			System.out.println();
 		}
-
-		String[] WORDS = words.toArray(new String[words.size()]);
-		double[][] tf = new double[sentences.length][WORDS.length];
-		double[] isf = new double[WORDS.length];
-		double[][] cosineScore = new double[sentences.length][sentences.length];
-		int countTF = 0;
-		int countISF = 0;
-
-		for (int i = 0; i < WORDS.length; i++) {
-			countISF = 0;
-			for (int j = 0; j < sentences.length; j++) {
-				boolean find = false;
-				countTF = 0;
-				for (int k = 0; k < senTokens[j].length; k++) {
-					if (senTokens[j][k].matches(WORDS[i])) {
-						countTF = countTF + 1;
-						find = true;
-					}
-
-				}
-				if (find == true) {
-					countISF = countISF + 1;
-				}
-				if (countTF != 0) {
-					tf[j][i] = 1 + Math.log10(Double.valueOf(countTF));
-				} else if (countTF == 0) {
-					tf[j][i] = 0;
-				}
-			}
-			if (countISF != 0) {
-				isf[i] = Math.log10(sentences.length / countISF);
-			} else if (countISF == 0) {
-				isf[i] = 0;
-			}
+				
+		Map<String, Integer> SimlarityDegree = new HashMap<String, Integer>();
+		int MaxDegree = 0;
+		int SentenceDegree = 0;
+		for(int i=0;i<CosineSimMatrix.length;i++)
+		{
+			for(int j=0;j<CosineSimMatrix[i].length;j++)
+				if(CosineSimMatrix[i][j] >= Threshold)
+					SentenceDegree++;
+		
+			SimlarityDegree.put(sentences[i], SentenceDegree);
+			if(SentenceDegree > MaxDegree)
+				MaxDegree = SentenceDegree;
 		}
-		for (int k = 0; k < sentences.length; k++) {
-			for (int i = 0; i < sentences.length; i++) {
-				double sum = 0, sqrt1 = 0, sqrt2 = 0;
-				if (i != k) {
-					for (int j = 0; j < WORDS.length; j++) {
-						sum += tf[k][j] * tf[i][j] * isf[j] * isf[j];
-						sqrt1 += (tf[k][j] * isf[j]) * (tf[k][j] * isf[j]);
-						sqrt2 += (tf[i][j] * isf[j]) * (tf[i][j] * isf[j]);
-					}
-
-					cosineScore[k][i] = sum / (Math.sqrt(sqrt1) * Math.sqrt(sqrt2));
-				} else if (i == k) {
-					cosineScore[k][i] = 0;
-				}
-			}
+		int k=0;
+		for(Map.Entry<String, Integer> entry : SimlarityDegree.entrySet())
+			{CentralityMatrix[k] = entry.getValue();k++;}
+		
+		return CentralityMatrix;
 		}
-
-		double[] senCentrality = new double[sentences.length];
-		int[] count = new int[sentences.length];
-		double max = 0;
-
-		for (int k = 0; k < sentences.length; k++) {
-			count[k] = 0;
-
-			for (int i = 0; i < sentences.length; i++) {
-				if (cosineScore[k][i] > 0.1) {
-					count[k]++;
-					if (count[k] > max) {
-						max = count[k];
-					}
-				}
-			}
-		}
-		for (int i = 0; i < sentences.length; i++) {
-			if (max == 0) {
-				senCentrality[i] = 0;
-			} else {
-				senCentrality[i] = count[i] / max;
-			}
-		}
-
-		return senCentrality;
-	}
 
 	private double[] sentenceLength(String[][] sentences_words) {
 		double[] sentences_score = new double[sentences_words.length];
