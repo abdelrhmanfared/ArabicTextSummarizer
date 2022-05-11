55a5df9258faeccc69d35948c4cb59c5079665b7
abdullahalaa12 <abdullah.alaa12@gmail.com>
2022-05-11 18:45:20 +0200

    Sentence Length Feature

---
+++ src/feature/SentenceLength.java
@@ -0,0 +1,84 @@
+package feature;
+
+import java.util.Arrays;
+
+import preprocessing.Preprocessing1;
+
+public class SentenceLength {
+	private double[] scoreBasedFeature;
+	private double[][] svmFetures;
+
+	/**
+	 * @return the scoreBasedFeature
+	 */
+	public double[] getScoreBasedFeature() {
+		return scoreBasedFeature;
+	}
+
+	/**
+	 * @return the svmFetures
+	 */
+	public double[][] getSvmFetures() {
+		return svmFetures;
+	}
+
+	public SentenceLength(Preprocessing1 pre) {
+		// TODO Auto-generated constructor stub
+		String[][] rootSentencesTokens = pre.getRootSentencesTokens();
+		scoreBasedFeature = new double[rootSentencesTokens.length];
+		svmFetures = new double[rootSentencesTokens.length][2];
+		int[] sentences_lengths = new int[rootSentencesTokens.length];
+
+		for (int i = 0; i < rootSentencesTokens.length; i++)
+			sentences_lengths[i] = rootSentencesTokens[i].length;
+
+		int[] help = sentenceLength_helper(sentences_lengths);
+		int _q1 = help[0], max = help[1], _q3 = help[2];
+
+		for (int i = 0; i < rootSentencesTokens.length; i++)
+			if (rootSentencesTokens[i].length > _q1 && rootSentencesTokens[i].length < _q3)
+				svmFetures[i][0] = scoreBasedFeature[i] = (double) rootSentencesTokens[i].length / max;
+			else
+				svmFetures[i][1] = 1;
+
+	}
+
+	// [0] = q1, [1] = max between q1 and q3, [2] = q3_index
+	private int[] sentenceLength_helper(int[] values) {
+		Arrays.sort(values);
+		int[] res = new int[3];
+
+		int mid_index = median(values, 0, values.length);
+
+		// Median of first half
+		int q1 = values[median(values, 0, mid_index)];
+
+		// Median of second half
+		int q3 = values[median(values, mid_index + 1, values.length)];
+
+		int iqr = q3 - q1;
+		int _q1 = q1 - (int) (1.5f * iqr);
+		int _q3 = q3 + (int) (1.5f * iqr);
+
+		int max = 0;
+		for (int i = 0; i < values.length; i++) {
+			if (values[i] > _q1 && values[i] < _q3)
+				if (values[i] > max)
+					max = values[i];
+		}
+
+		res[0] = _q1;
+		res[1] = max;
+		res[2] = _q3;
+		return res;
+	}
+
+	// Function to give
+	// index of the median
+	static int median(int a[], int l, int r) {
+		int n = r - l + 1;
+		n = (n + 1) / 2 - 1;
+		return n + l;
+	}
+
+}
