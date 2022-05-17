27905672b3f8d3f0a02bf1c62c9e41c2578d5bb3
abdullahalaa12 <abdullah.alaa12@gmail.com>
2022-05-17 17:34:43 +0200

    main

--- src/program/Main.java
+++ src/program/Main.java
@@ -1,9 +1,8 @@
 package program;
 
-import textrank.Textrank;
 import utilities.SentenceDetector;
-import java.util.ArrayList;
 
+import featuresextraction.FeaturesExtraction;
 import file.AccuracyFile;
 import file.AverageFile;
 import file.DatasetFile;
@@ -12,39 +11,38 @@ import file.DatasetFile;
 public class Main {
 
 	public static void main(String[] args) {
-
 		// TODO Auto-generated method stub
 		try {
-			// PREPROCESSING TESTING !!!
-			// String ArabicText = ArabicTextFile.getArabicText();
-			// Preprocessing1 pre1 = new Preprocessing1(ArabicText);
-			// Preprocessing pre = new Preprocessing(ArabicText);
-
 			DatasetFile datasetFile = new DatasetFile();
-			AccuracyFile acFile;
-			AverageFile averageFile = new AverageFile();
-
-			ArrayList<String> TextRankSummary = new ArrayList<String>();
+//			AccuracyFile acFile;
+//			AverageFile averageFile = new AverageFile();
+//
+//			ArrayList<String> TextRankSummary = new ArrayList<String>();
 			SentenceDetector sd = new SentenceDetector();
 
-			for (int i = 0; i < 75; i++) {
+			for (int i = 0; i < datasetFile.size(); i++) {
 				String Article = datasetFile.getArticles().get(i);
 				String Title = datasetFile.getTitles().get(i);
 				String Summary = datasetFile.getSummaries().get(i);
-				
+
 				System.out.println("Summarizing Article " + Title + " #" + i);
 
+				// Testing Features !
 
 				int SUMMARY_NO_SENTENCES = sd.detectSentences(Summary).length;
-				Textrank textrank = new Textrank(Article, Title, SUMMARY_NO_SENTENCES);
-				String GeneratedSummary = textrank.getSummarizedText();
-				
-				TextRankSummary.add(GeneratedSummary);
+				FeaturesExtraction fe = new FeaturesExtraction(Article, Title);
+
+				// End Testing
+//				int SUMMARY_NO_SENTENCES = sd.detectSentences(Summary).length;
+//				Textrank textrank = new Textrank(Article, Title, SUMMARY_NO_SENTENCES);
+//				String GeneratedSummary = textrank.getSummarizedText();
+//
+//				TextRankSummary.add(GeneratedSummary);
 			}
-			acFile = new AccuracyFile("TextRank", averageFile);
-			acFile.CreateAccuracySheet(TextRankSummary, datasetFile);
-			acFile = new AccuracyFile("SVM", averageFile);
-			acFile.CreateAccuracySheet(TextRankSummary, datasetFile);
+//			acFile = new AccuracyFile("TextRank", averageFile);
+//			acFile.CreateAccuracySheet(TextRankSummary, datasetFile);
+//			acFile = new AccuracyFile("SVM", averageFile);
+//			acFile.CreateAccuracySheet(TextRankSummary, datasetFile);
 
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
